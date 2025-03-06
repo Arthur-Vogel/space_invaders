08f067e78f4bad65b2a567481a8c6ce6b43f3d3e
Arthur-Vogel <arthurgrasse@outlook.fr>
2025-03-05 23:44:47 -0800

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public GameObject bullet;
+
+  public Transform shottingOffset;
+    // Update is called once per frame
+    private void Start()
+    {
+      Enemy.OnEnemyDied += EnemyOnOnEnemyDied;
+    }
+    void OnDestroy()
+    {
+      Enemy.OnEnemyDied -= EnemyOnOnEnemyDied;
+    }
+    void EnemyOnOnEnemyDied(int points)
+    {
+      Debug.Log("Yay! I got points :" + points);
+    }
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+        Debug.Log("Bang!");
+
+        //Destroy(shot, 3f);
+
+      }
+    }
+}

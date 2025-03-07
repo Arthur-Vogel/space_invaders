0af8a8adce9f0dc09807665846a1bae61f20a512
Arthur-Vogel <arthurgrasse@outlook.fr>
2025-03-07 09:42:04 -0800

    space invaders pt1 late

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,59 @@ public class Enemy : MonoBehaviour
     
     public delegate void EnemyDied(int points);
     public static event EnemyDied OnEnemyDied;
+    private int _movement = 0;
+    public int max_right = 5;
+    public int max_left = -5;
+    public float speed = 2f;
+    public float downSpeed = 0.75f;
+    public int points = 300;
+    public float time=10;
+
+    private float decr;
+    /*public int type_of_ennemy;
+    Enemy(int typeOfEnnemy)
+    {
+      type_of_ennemy = typeOfEnnemy;
+    }*/
+    private void Update()
+    {
+        
+      if(_movement == 1)
+      {
+        transform.position += Vector3.left * (Time.deltaTime * speed);
+        if(decr <= 0)
+        {
+          _movement = 0;
+          transform.position += Vector3.down *downSpeed;
+          decr = time;
+        }
+        
+      }
+      if(_movement == 0)
+      {
+        transform.position += Vector3.right * (Time.deltaTime * speed);
+        if(decr <= 0)
+        {
+          _movement = 1;
+          transform.position += Vector3.down * downSpeed;
+          decr = time;
+        }
+      }
+      decr-= Time.deltaTime;
+    }
+
+    private void Start()
+    {
+      decr = time;
+      
+    }
+
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D collision)
     {
       Debug.Log("Ouch!");
       Destroy(collision.gameObject);
-      if (OnEnemyDied != null) OnEnemyDied.Invoke( 300);
+      if (OnEnemyDied != null) OnEnemyDied.Invoke( points);
+      Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
   public GameObject bullet;
 
   public Transform shottingOffset;
-    // Update is called once per frame
+  
+  public float speed = 5f;
+  private Rigidbody2D rb;
+  private float moveInput;
+  private int score = 0;
+  Animator playerAnimator;
+  public Text scoreText;
+  
+
+  private static readonly int Property = Animator.StringToHash("Shoot trigger");
+
+  // Update is called once per frame
     private void Start()
     {
       Enemy.OnEnemyDied += EnemyOnOnEnemyDied;
+      playerAnimator = GetComponent<Animator>();
+      rb = GetComponent<Rigidbody2D>();
+      scoreText.text = score.ToString();
     }
     void OnDestroy()
     {
@@ -19,17 +35,23 @@ public class Player : MonoBehaviour
     }
     void EnemyOnOnEnemyDied(int points)
     {
-      Debug.Log("Yay! I got points :" + points);
+      score += points;
+      scoreText.text = score.ToString();
     }
+    [Obsolete("Obsolete")]
     void Update()
     {
       if (Input.GetKeyDown(KeyCode.Space))
       {
+        playerAnimator.SetTrigger(Property);
         GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
         Debug.Log("Bang!");
 
         //Destroy(shot, 3f);
 
       }
+      moveInput = Input.GetAxisRaw("Horizontal");
+      rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+      
     }
 }
